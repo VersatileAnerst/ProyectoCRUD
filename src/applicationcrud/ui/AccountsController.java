68a8a57640279e370489ec0982c69edd9c0f0346
VersatileAnerst <151840641+VersatileAnerst@users.noreply.github.com>
2026-01-13 19:41:47 +0100

    Test Account table
    Functrion to test the table

--- src/applicationcrud/ui/AccountsController.java
+++ src/applicationcrud/ui/AccountsController.java
@@ -5,13 +5,15 @@
  */
 package applicationcrud.ui;
 
+import applicationcrud.logic.AccountRESTClient;
 import applicationcrud.model.Account;
 import applicationcrud.model.Customer;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleLongProperty;
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,6 +22,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -29,7 +32,7 @@ import javafx.stage.Stage;
 public class AccountsController {
 
     @FXML
-    private TableView <Account> tAccounts;
+    private TableView<Account> tAccounts;
     @FXML
     private TableColumn<Account, Long> colId;
     @FXML
@@ -60,6 +63,10 @@ public class AccountsController {
     private Stage stage;
 
     private Customer customer;
+    
+    private AccountRESTClient accountClient;
+    
+    private ObservableList<Account> accountsData;
 
     private static final Logger LOGGER = Logger.getLogger("applicationcrud.ui");
 
@@ -72,29 +79,25 @@ public class AccountsController {
             stage.setTitle("BankApp");
             //Ventana no redimensionable
             stage.setResizable(false);
-            //Asociación de manejadores a properties
-
-            colId.setCellValueFactory(cell
-                    -> new SimpleLongProperty(cell.getValue().getId()).asObject());
-
-            colDescription.setCellValueFactory(cell     
-                    -> new SimpleStringProperty(cell.getValue().getDescription()));
-
-            colBalance.setCellValueFactory(cell
-                    -> new SimpleDoubleProperty(cell.getValue().getBalance()).asObject());
-
-            colCreditLine.setCellValueFactory(cell
-                    -> new SimpleDoubleProperty(cell.getValue().getCreditLine()).asObject());
-
-            colBeginBalance.setCellValueFactory(cell
-                    -> new SimpleDoubleProperty(cell.getValue().getBeginBalance()).asObject());
-
-            colType.setCellValueFactory(cell
-                    -> new SimpleStringProperty(cell.getValue().getType().name()));
-
+            //Factorias para el valor de celda
+            colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+            colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+            colBalance.setCellValueFactory(new PropertyValueFactory<>("balance"));
+            colCreditLine.setCellValueFactory(new PropertyValueFactory<>("creditLine"));
+            colBeginBalance.setCellValueFactory(new PropertyValueFactory<>("beginBalance"));
+            colType.setCellValueFactory(new PropertyValueFactory<>("type"));
+            colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+            //Listener
+            tAccounts.getSelectionModel().selectedItemProperty()
+                    .addListener(this::handleAccountsTableSelectionChanged);
 
             //Establecer el botón de Exit como cancelButton. 
             btExit.setCancelButton(true);
+            // Botones deshabilitados
+            btUpdate.setDisable(true);
+            btDelete.setDisable(true);
+            btMovement.setDisable(true);
+
             //Mostrar la ventana
             stage.show();
             LOGGER.info("Account window initialized");
@@ -108,7 +111,53 @@ public class AccountsController {
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
-        lbMessage.setText("Welcome " + customer.getEmail());
+        loadTestData();
     }
+
+    private void loadAccounts() {
+        try {
+            accountClient = new AccountRESTClient();
+
+            List<Account> accountList =
+                    accountClient.findAccountsByCustomerId_XML(
+                            List.class,
+                            customer.getId().toString());
+
+            accountsData = FXCollections.observableArrayList(accountList);
+
+            tAccounts.setItems(accountsData);
+        } catch (Exception e) {
+            LOGGER.warning(e.getLocalizedMessage());
+            new Alert(Alert.AlertType.ERROR,
+                    "Error loading Accounts: " + e.getLocalizedMessage())
+                    .showAndWait();
+        }
+    }
+    private void handleAccountsTableSelectionChanged(ObservableValue observable,
+                                                     Object oldValue,
+                                                     Object newValue) {
+
+        if (newValue != null) {
+            btUpdate.setDisable(false);
+            btDelete.setDisable(false);
+            btMovement.setDisable(false);
+        }
+    }
+    private void loadTestData() {
+    accountsData = FXCollections.observableArrayList();
+
+    Account a = new Account();
+    a.setId(1L);
+    a.setDescription("Account 1 Test");
+    a.setBalance(1000.0);
+    a.setBeginBalance(1000.0);
+    a.setCreditLine(0.0); // Cuenta estándar
+    a.setType(applicationcrud.model.AccountType.STANDARD);
+    a.setBeginBalanceTimestamp(new java.util.Date());
+
+    accountsData.add(a);
+ 
+    tAccounts.setItems(accountsData);
+}
 
 }
